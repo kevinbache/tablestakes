fda4ea4236201574bf4f517b32a2d1f52ee5071a
Kevin Bache <kevin.bache@gmail.com>
2020-03-09 20:46:49 -0600

    removed cruft

--- tablestakes/__init__.py
+++ tablestakes/__init__.py
@@ -1,5 +1,5 @@
-"""tablestakes - Brown, paper, tied up with string."""
+"""tablestakes - I mean, why not?"""
 
 __version__ = '0.0.1'
-__author__ = 'My Name <kevin.bache@gmail.com>'
+__author__ = 'Kevin Bache <kevin.bache@gmail.com>'
 __all__ = []
